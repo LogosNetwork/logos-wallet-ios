560245da183845b5cc83f0e42589a79796d8c231
Benjamin Kray <ben.kray@prometheanlabs.io>
2018-12-06 21:47:25 -0800

    Changed default node url

--- LogosWallet/LogosWallet/Logos/RealmModel/AppUrl.swift
+++ LogosWallet/LogosWallet/Logos/RealmModel/AppUrl.swift
@@ -10,7 +10,7 @@ import RealmSwift
 
 class AppUrl: Object {
 
-    @objc dynamic var nodeUrl: String = "http://52.215.106.54:55000"
+    @objc dynamic var nodeUrl: String = "http://107.21.165.224:55000"
     @objc dynamic var walletServerUrl: String = "ws://18.202.143.35:443"
 
 }
