75ac033a269a1773bbe1acb9126c99e69e48ff49
Benjamin Kray <ben.kray@prometheanlabs.io>
2019-03-21 23:45:41 -0700

    Added Logos Store

---
+++ LogosWallet/LogosWallet/Logos/LogosStore.swift
@@ -0,0 +1,32 @@
+//
+//  LogosStore.swift
+//  LogosWallet
+//
+//  Created by Ben Kray on 3/21/19.
+//  Copyright © 2019 Planar Form. All rights reserved.
+//
+
+import Foundation
+
+class LogosStore {
+
+    enum StorageKey: String {
+        case accounts
+        case transactions
+    }
+
+    fileprivate init() { }
+
+    static func getAccounts() -> [LogosAccountInfo] {
+        if let contents = try? FileManager.default.contentsOfDirectory(atPath: Storage.documentsUrl.path + "/\(StorageKey.accounts.rawValue)") {
+            return contents.compactMap { Storage.retrieve(StorageKey.accounts.rawValue + "/\($0)", as: LogosAccountInfo.self) }
+        } else {
+            return []
+        }
+    }
+
+    static func update(account: String, info: LogosAccountInfo) {
+        Storage.store(info, as: account, directoryPath: StorageKey.accounts.rawValue)
+    }
+
+}
