d9b3a2a1255cba535218ff7a3e30450139278048
Benjamin Kray <ben.kray@prometheanlabs.io>
2019-03-30 23:20:13 -0700

    Added carousel cell and flow layout

---
+++ LogosWallet/LogosWallet/ViewControllers/Account/AccountCarouselCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AccountCarouselCollectionViewCell.swift
+//  LogosWallet
+//
+//  Created by Ben Kray on 3/30/19.
+//  Copyright © 2019 Planar Form. All rights reserved.
+//
+
+import UIKit
+
+class AccountCarouselCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ LogosWallet/LogosWallet/ViewControllers/Account/CarouselViewFlowLayout.swift
@@ -0,0 +1,9 @@
+//
+//  CarouselFlowLayout.swift
+//  LogosWallet
+//
+//  Created by Ben Kray on 3/30/19.
+//  Copyright © 2019 Planar Form. All rights reserved.
+//
+
+import Foundation
